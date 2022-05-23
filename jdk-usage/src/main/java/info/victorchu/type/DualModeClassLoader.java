405139136fb084d9f6a077e0d8d239afdea59807
chutian0610 <victorchu0610@outlook.com>
2022-05-24 00:47:04 +0800

    feat(jdk): classorder

--- jdk-usage/src/main/java/info/victorchu/type/DualModeClassLoader.java
+++ jdk-usage/src/main/java/info/victorchu/type/DualModeClassLoader.java
@@ -1,9 +1,62 @@
 package info.victorchu.type;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
  * 双模类加载器。
  * 1. parent first 模式。
  * 2. child first 模式。
  */
-public class DualModeClassLoader {
+public class DualModeClassLoader extends URLClassLoader {
+    static {
+        ClassLoader.registerAsParallelCapable();
+    }
+
+    private final String[] alwaysParentFirstPatterns;
+
+    private Boolean parentFirst;
+
+    public DualModeClassLoader(URL[] urls, ClassLoader parent, String[] alwaysParentFirstPatterns, Boolean parentFirst) {
+        super(urls, parent);
+        this.alwaysParentFirstPatterns = alwaysParentFirstPatterns;
+        this.parentFirst = parentFirst;
+    }
+
+    @Override
+    public final Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        if(parentFirst){
+          return loadClassParentFirst(name, resolve);
+        }else{
+          return loadClassChildFirst(name,resolve);
+        }
+    }
+    public final Class<?> loadClassChildFirst(String name, boolean resolve) throws ClassNotFoundException {
+        // First, check if the class has already been loaded
+        Class<?> c = findLoadedClass(name);
+        if (c == null) {
+            // check whether the class should go parent-first
+            for (String alwaysParentFirstPattern : alwaysParentFirstPatterns) {
+                if (name.startsWith(alwaysParentFirstPattern)) {
+                    return super.loadClass(name, resolve);
+                }
+            }
+            try {
+                // check the URLs
+                c = findClass(name);
+            } catch (ClassNotFoundException e) {
+                // let URLClassLoader do it, which will eventually call the parent
+                c = super.loadClass(name, resolve);
+            }
+        } else if (resolve) {
+            resolveClass(c);
+        }
+
+        return c;
+    }
+
+
+    public final Class<?> loadClassParentFirst(String name, boolean resolve) throws ClassNotFoundException {
+        return super.loadClass(name, resolve);
+    }
 }
