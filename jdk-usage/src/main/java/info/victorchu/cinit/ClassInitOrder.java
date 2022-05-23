b3c43535f13b51f7f2d8b1a3dc4525d0a7c4d1a3
chutian0610 <victorchu0610@outlook.com>
2022-05-23 19:31:20 +0800

    feat(jdk): Cinit order

---
+++ jdk-usage/src/main/java/info/victorchu/cinit/ClassInitOrder.java
@@ -0,0 +1,37 @@
+package info.victorchu.cinit;
+
+public class ClassInitOrder {
+
+    public static void main(String[] args) {
+        Parent parent = new Child();
+    }
+}
+
+class Sample {
+
+    String s;
+
+    Sample(String s) {
+        this.s = s;
+        System.out.println(s);
+    }
+    Sample(String s,String old) {
+        this.s = s;
+        System.out.println(s+"->"+old);
+    }
+
+    static Sample init(Sample s,String str) {
+        if(s == null){
+            return new Sample(str);
+        }else {
+            return new Sample(str,s.toString());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.s;
+    }
+}
+
+
