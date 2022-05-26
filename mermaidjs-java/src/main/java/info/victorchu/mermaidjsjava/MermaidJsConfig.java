e98a2a8c54c4f94e940e48e3f3277f6e16625e67
chutian0610 <victorchu0610@outlook.com>
2022-05-26 15:23:37 +0800

    更新CDN

--- mermaidjs-java/src/main/java/info/victorchu/mermaidjsjava/MermaidJsConfig.java
+++ mermaidjs-java/src/main/java/info/victorchu/mermaidjsjava/MermaidJsConfig.java
@@ -8,7 +8,8 @@ import lombok.Setter;
 public class MermaidJsConfig {
 
     public static final MermaidJsConfig defaultConfig = new MermaidJsConfig();
-
-    private String cdn = "https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js";
+//
+//    private String cdn = "https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"; //jsdelivr cdn
+    private String cdn = "https://npm.elemecdn.com/mermaid@8.13.10/dist/mermaid.min.js"; // 饿了么 unpkg镜像
 
 }
