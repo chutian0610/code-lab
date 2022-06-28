5c16df0f686397c0663fde867321d594be91a9ec
chutian0610 <victorchu0610@outlook.com>
2022-06-28 18:30:40 +0800

    M: javadoc

---
+++ compiler-study/src/main/java/info/victorchu/compiler/simpleregex/automata/NFABeginState.java
@@ -0,0 +1,8 @@
+package info.victorchu.compiler.simpleregex.automata;
+
+/**
+ * @author victorchu
+ * @date 2022/6/28 6:15 下午
+ */
+public class NFABeginState extends NFAState {
+}
--- compiler-study/src/test/java/info/victorchu/compiler/simpleregex/RegexParserTest.java
+++ compiler-study/src/test/java/info/victorchu/compiler/simpleregex/RegexParserTest.java
@@ -1,20 +1,11 @@
 package info.victorchu.compiler.simpleregex;
 
-import info.victorchu.compiler.simpleregex.util.RegexNodeDrawVisitor;
 import info.victorchu.compiler.simpleregex.util.RegexNodePrintVisitor;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 class RegexParserTest {
-
-    @Test
-    void draw() throws IOException {
-        RegexNode regexNode= RegexParser.parse("ab*c|bc");
-        RegexNodeDrawVisitor visitor = new RegexNodeDrawVisitor();
-        regexNode.accept(visitor);
-        visitor.draw();
-    }
     @Test
     void parse() throws IOException {
         RegexNode regexNode= RegexParser.parse("ab*c|bc");
