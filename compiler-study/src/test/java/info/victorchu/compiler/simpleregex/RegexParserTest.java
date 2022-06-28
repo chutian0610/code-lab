805127c69570c66ea23d291ac186d5e2e8741399
chutian0610 <victorchu0610@outlook.com>
2022-06-28 16:38:46 +0800

    add tool

--- compiler-study/src/test/java/info/victorchu/compiler/simpleregex/RegexParserTest.java
+++ compiler-study/src/test/java/info/victorchu/compiler/simpleregex/RegexParserTest.java
@@ -1,6 +1,7 @@
 package info.victorchu.compiler.simpleregex;
 
 import info.victorchu.compiler.simpleregex.util.RegexNodeDrawVisitor;
+import info.victorchu.compiler.simpleregex.util.RegexNodePrintVisitor;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -8,10 +9,17 @@ import java.io.IOException;
 class RegexParserTest {
 
     @Test
-    void parse() throws IOException {
+    void draw() throws IOException {
         RegexNode regexNode= RegexParser.parse("ab*c|bc");
         RegexNodeDrawVisitor visitor = new RegexNodeDrawVisitor();
         regexNode.accept(visitor);
         visitor.draw();
+    }
+    @Test
+    void parse() throws IOException {
+        RegexNode regexNode= RegexParser.parse("ab*c|bc");
+        RegexNodePrintVisitor visitor = new RegexNodePrintVisitor();
+        regexNode.accept(visitor);
+        System.out.println(visitor.build());
     }
 }
