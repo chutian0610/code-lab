727881ef08110826ec1fef0a3a37933e8f2a4033
chutian0610 <victorchu0610@outlook.com>
2022-07-09 19:16:49 +0800

    M: tmp commit

--- compiler-study/src/main/java/info/victorchu/compiler/simpleregex/automata/NFA.java
+++ compiler-study/src/main/java/info/victorchu/compiler/simpleregex/automata/NFA.java
@@ -9,21 +9,5 @@ import java.util.List;
  * @date 2022/6/28 5:26 下午
  */
 public class NFA {
-    private final List<NFAState> stateList = new ArrayList<>();
-
-    public NFA(){
-        stateList.add(new NFABeginState());
-    }
-
-    public List<NFAState> getStateList() {
-        return stateList;
-    }
-
-    public void addState(NFAState nfaState){
-        stateList.add(nfaState);
-    }
-
-    public NFAState getLastNFAState(){
-        return stateList.get(stateList.size()-1);
-    }
+   
 }
