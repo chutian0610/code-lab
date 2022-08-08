710b1c04f3db07a4201bd0aaa0241e98c5f6acfb
chutian0610 <victorchu0610@outlook.com>
2022-08-08 20:33:26 +0800

    feat: add test for producer & consumer Future

---
+++ common-tool/src/test/java/info/victorchu/commontool/tasks/pcfuture/TaskTest.java
@@ -0,0 +1,40 @@
+package info.victorchu.commontool.tasks.pcfuture;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author victorchu
+ * @date 2022/8/8 20:32
+ */
+@Slf4j
+class TaskTest {
+    @Test
+    public void test1() throws ExecutionException, InterruptedException {
+        TaskLimitBlockQueue queue = new TaskLimitBlockQueue(16);
+        TaskConsumer consumer = new TaskConsumer(queue);
+        consumer.start();
+        for (int i = 0; i < 25; i++) {
+            Task task = new TaskImpl();
+            if(queue.offer(task)){
+                log.info("task{} offered",task.getName());
+            }else{
+                log.info("task{} drop",task.getName());
+            }
+        }
+        Thread.sleep(3000);
+        Task task = new TaskImpl();
+        if(queue.offer(task)) {
+            log.info("task{} offered",task.getName());
+            task.get();
+        }else {
+            log.info("task{} drop",task.getName());
+        }
+
+
+    }
+}
