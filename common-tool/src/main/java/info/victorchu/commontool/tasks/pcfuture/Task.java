16ab6a69583c441f6c1ac8dba57321dcf93abfb2
chutian0610 <victorchu0610@outlook.com>
2022-08-08 11:33:59 +0800

    feat: fix antlr memory leak

---
+++ common-tool/src/main/java/info/victorchu/commontool/tasks/pcfuture/Task.java
@@ -0,0 +1,14 @@
+package info.victorchu.commontool.tasks.pcfuture;
+
+/**
+ * @author victorchu
+ * @date 2022/8/5 22:24
+ */
+public interface Task {
+
+    /**
+     * task 的资源权重
+     * @return
+     */
+    Integer getWeight();
+}
