982ae4f9736c52f08aaf419128927add679fab25
chutian0610 <victorchu0610@outlook.com>
2022-08-10 23:36:00 +0800

    feat: add test for producer & consumer Future

--- common-tool/src/main/java/info/victorchu/commontool/tuple/simple/package-info.java
+++ common-tool/src/main/java/info/victorchu/commontool/tuple/simple/package-info.java
@@ -1,6 +1,6 @@
 /**
  * A simple type-safe Tuple implementation.
- * Taken from http://stackoverflow.com/questions/3642452/java-n-tuple-implementation/3642623#3642623
+ * @see <a href="http://stackoverflow.com/questions/3642452/java-n-tuple-implementation/3642623#3642623">stackoverflow link</a>
  * @date 2022/8/10 23:28
  * @author victorchu
  */
