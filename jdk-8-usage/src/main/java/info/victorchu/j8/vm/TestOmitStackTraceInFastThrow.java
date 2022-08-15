6fc0679d66fe06831681b63f0c63294a0fd6ddba
chutian0610 <victorchu0610@outlook.com>
2022-08-15 20:09:02 +0800

    feat: OmitStackTraceInFastThrow demo

--- jdk-8-usage/src/main/java/info/victorchu/j8/vm/TestOmitStackTraceInFastThrow.java
+++ jdk-8-usage/src/main/java/info/victorchu/j8/vm/TestOmitStackTraceInFastThrow.java
@@ -1,6 +1,15 @@
 package info.victorchu.j8.vm;
 
 /**
+ * <pre>
+ *   JVM对一些特定的异常类型做了Fast Throw优化，
+ *   如果检测到在代码里某个位置连续多次抛出同一类型异常的话，C2会决定用Fast Throw方式来抛出异常，
+ *   而异常Trace即详细的异常栈信息会被清空。
+ *
+ *   抛出了几千次带有详细异常栈信息的异常后，只会抛出java.lang.NullPointerException这种没有详细异常栈信息只有异常类型的异常信息。
+ *   这就是Fast Throw优化后抛出的异常。
+ *   如果我们配置了-XX:-OmitStackTraceInFastThrow，再次运行，就不会看到Fast Throw优化后抛出的异常，全是包含了详细异常栈的异常信息。
+ * </pre>
  * @author victorchu
  * @date 2022/8/15 19:57
  */
