fdb0a6a519eed9f5284eb117ca4d2bec21bc5323
chutian0610 <victorchu0610@outlook.com>
2023-03-28 18:54:38 +0800

    docs: 静态初始化顺序

---
+++ jdk-8-usage/src/main/java/info/victorchu/j8/type/cinit/demo/order/ClassStaticInit.java
@@ -0,0 +1,30 @@
+package info.victorchu.j8.type.cinit.demo.order;
+
+public class ClassStaticInit {
+
+    public static void main(String[] args) {
+        staticFunction();
+    }
+
+    static ClassStaticInit st = new ClassStaticInit();
+
+    static {   //静态代码块
+        System.out.println("1");
+    }
+
+    {       // 实例代码块
+        System.out.println("2");
+    }
+
+    ClassStaticInit() {    // 实例构造器
+        System.out.println("3");
+        System.out.println("a=" + a + ",b=" + b);
+    }
+
+    public static void staticFunction() {   // 静态方法
+        System.out.println("4");
+    }
+
+    int a = 110;    // 实例变量
+    static int b = 112;     // 静态变量
+}
