95a77b0ebf967e1361f7852726051518664dca7b
chutian0610 <victorchu0610@outlook.com>
2023-06-02 15:35:02 +0800

    A: jvm

--- jvm-all/jvm8test/src/main/java/info/victorchu/jvm/j8/mem/VMStack.java
+++ jvm-all/jvm8test/src/main/java/info/victorchu/jvm/j8/mem/VMStack.java
@@ -4,16 +4,12 @@ public class VMStack {
     private static int increaseByTen(int c){
         return c + 10;
     }
-
-    private static int multiplyByTen(int c){
-        return c * 10;
-    }
-
-    public static void main(String[] args) {
-        int a, b, c;
+    public void test() {
+        long c;
+        int a, b;
         a = 1;
         b = 2;
         c = increaseByTen(a*b);
-        c = multiplyByTen(c)*(a+b);
+        c = c*(a+b);
     }
 }
