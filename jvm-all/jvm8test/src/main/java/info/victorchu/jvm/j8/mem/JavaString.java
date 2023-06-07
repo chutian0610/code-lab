4bfa92296a0943868405220855cadc5753098aff
chutian0610 <victorchu0610@outlook.com>
2023-06-07 10:15:05 +0800

    M

--- jvm-all/jvm8test/src/main/java/info/victorchu/jvm/j8/mem/JavaString.java
+++ jvm-all/jvm8test/src/main/java/info/victorchu/jvm/j8/mem/JavaString.java
@@ -1,28 +1,12 @@
 package info.victorchu.jvm.j8.mem;
 
-/**
- * 查看字节码中
- * 1. JVM对String的存放位置，运行时常量池,StringTable和堆中
- * 2. JVM中”+“的处理
- * <pre>
- * cd jvm-all/jvm8test/target/classes/info/victorchu/jvm/j8/mem
- * javap -verbose JavaString
- * </pre>
- */
 public class JavaString {
-    public static void main(String[] args) {
-        String baseStr = "baseStr";
-        final String baseFinalStr = "baseStr";
+    public static void main(String[] args){
+        String str1 = "string";
+        String str2 = new String("string");
+        String str3 = str2.intern();
 
-        String str1 = "baseStr01";
-        String str2 = "baseStr" + "01";
-        String str3 = baseStr + "01";
-        String str4 = baseFinalStr + "01";
-        String str5 = new String("baseStr01").intern();
-
-        System.out.println(str1 == str2);
-        System.out.println(str1 == str3);
-        System.out.println(str1 == str4);
-        System.out.println(str1 == str5);
+        System.out.println(str1==str2);
+        System.out.println(str1==str3);
     }
 }
---
+++ jvm-all/jvm8test/src/main/java/info/victorchu/jvm/j8/mem/JavaStringAdd01.java
@@ -0,0 +1,12 @@
+package info.victorchu.jvm.j8.mem;
+
+public class JavaStringAdd01 {
+    
+    public static void main(String[] args) {
+        String str1 = "baseStr01";
+        String str2 = "base"+"Str"+"01";
+        String str3 = "baseStr"+new String("01");
+        System.out.println(str2 == str1);
+        System.out.println(str2 == str3);
+    }
+}
