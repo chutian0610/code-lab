de6636ed2dc84f1979c710d645c69d500bda0b67
chutian0610 <victorchu0610@outlook.com>
2026-06-25 22:47:51 +0800

    chore(java-snippets): 删除未被引用的 shared 线程池实现
    concurrency/threadpool/shared/ 下 7 个文件(712 行)自上次实验后已无任何引用,
    也未被新模块使用,清理:
    
    - MultiQueueDualSemaphore / PrioritizedListenableFutureTask
    - ResourceGroup / ResourceGroupManaged / ResourceGroupManager
    - ResourceManagedTask / ResourceSharedPool
    
    验证: `grep -rn 'class-name' snippets/` 无命中

--- snippets/java-snippets/src/main/java/info/victorchu/snippets/concurrency/threadpool/shared/ResourceGroupManaged.java
+++
@@ -1,7 +0,0 @@
-package info.victorchu.snippets.concurrency.threadpool.shared;
-
-import info.victorchu.snippets.concurrency.threadpool.priority.Prioritized;
-
-public interface ResourceGroupManaged extends Prioritized {
-    String getResourceGroupId();
-}
